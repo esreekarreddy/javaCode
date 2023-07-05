365fc1111649494d7954c9b57f1343c2df97c777
esreekarreddy <esreekarreddy@gmail.com>
2023-07-05 21:35:10 +0530

    streams example

--- src/stream/Names.java
+++ src/stream/Names.java
@@ -1,7 +1,10 @@
 package stream;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Names {
@@ -12,8 +15,9 @@ public class Names {
 		names.add("Sreekar");
 		names.add("Shiva");
 		names.add("Chitra");
-		names.add("Padma");
+		names.add("Padmavathi");
 		
+		//returns true or false no matter the condition
 		long d = Stream.of("Ravi", "Kamal").filter(s-> 
 		{
 			s.startsWith("s");
@@ -21,8 +25,45 @@ public class Names {
 		}).count();
 		System.out.println(d);
 		
+		//to print count of names starting with S
 		long count = names.stream().filter(s->s.startsWith("S")).count();
 		System.out.println(count);
+		
+		//to print all names in the list whose length greater than 5
+		names.stream().filter(s->s.length()>5).forEach(s->System.out.println(s));
+		
+		//print upper case names with last letter with A
+		Stream.of("Sreekar", "Padma", "Shiva")
+			.filter(s->s.endsWith("a"))
+			.map(s->s.toUpperCase())
+			.forEach(s->System.out.println(s));
+		
+		//print first letter A name sorted way
+		List<String> namesList = Arrays.asList("Az", "Abhishek", "Abhilash", "Kiran", "Kumar");
+		namesList.stream()
+			.filter(s->s.startsWith("A"))
+			.sorted()
+			.map(s->s.toUpperCase())
+			.forEach(s->System.out.println(s));
+		
+		//merging two streams
+		Stream.concat(namesList.stream(), Stream.of("Sreekar","Nagraj"))
+			.sorted()
+			.forEach(s->System.out.println(s));
+		
+		//collect into a list
+		
+		List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 1, 2, 3, 22, 11, 4, 5, 6, 6, 7);
+		//sort the array
+		List<Integer> sorted = numbers.stream()
+			.sorted()
+			.collect(Collectors.toList());
+		System.out.println(sorted);
+		//print unique numbers
+		Set<Integer> unique = numbers.stream()
+									.collect(Collectors.toSet());
+		System.out.println(unique);
+		
 
 	}
 
